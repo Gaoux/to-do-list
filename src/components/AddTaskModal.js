e589368bc02d47a5eabb99eb9f175988e735e795
Gustavo Parra <gustavoparratanoux@gmail.com>
2023-06-21 00:55:26 -0500

    Finish the desgin of add tsak modal

--- src/components/AddTaskModal.js
+++ src/components/AddTaskModal.js
@@ -1,18 +1,51 @@
 import Modal from 'react-bootstrap/Modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+
 function AddTaskModal(props) {
+  const [dateExpired, setDateExpired] = useState(false);
+  const [selectedDate, setSelectedDate] = useState('(Today)');
   const handleClose = () => props.setShow(false);
   // const handleShow = () => props.setShow(true);
+
+  //Curent date
+  var curr = new Date();
+  curr.setDate(curr.getDate() + 3);
+  var date = curr.toISOString().substring(0, 10);
+
+  const handleDateChange = (e) => {
+    //Date selected on input
+    var dateSelected = new Date(e.target.value);
+    //Calculate Difference in days from the selected date and the actual date
+    var Difference_In_Time = dateSelected.getTime() - new Date().getTime();
+    var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+    Difference_In_Days = Math.round(Difference_In_Days);
+
+    if (Difference_In_Days < 0) {
+      setDateExpired(true);
+      setSelectedDate('');
+    } else {
+      setDateExpired(false);
+      if (Difference_In_Days === 0) setSelectedDate('(Today)');
+      else if (Difference_In_Days === 1) setSelectedDate('(Tomorrow)');
+      else if (Difference_In_Days < 7) setSelectedDate('(This week)');
+      else if (Difference_In_Days < 30) setSelectedDate('(This month)');
+      else if (Difference_In_Days < 60) setSelectedDate('(Next month)');
+      else setSelectedDate('');
+    }
+  };
 
   return (
     <>
       <Modal
+        className="add-task-modal"
+        isOpen={props.show}
         show={props.show}
         onHide={handleClose}
-        backdrop="static"
         keyboard={false}
-        className="add-task-modal"
+        backdrop="static"
+        closeTimeoutMS={500}
       >
         <Modal.Header className="add-task-modal__header">
           <Modal.Title>Add a task</Modal.Title>
@@ -23,31 +56,58 @@ function AddTaskModal(props) {
           />
         </Modal.Header>
         <Modal.Body className="add-task-modal__body">
-          <form class=" rounded px-8 pt-6 pb-8 mb-4">
-            <div class="mb-4">
+          <form className=" rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
               <label
-                class="block text-gray-700 text-sm font-bold mb-2"
+                className="block text-gray-700 text-sm font-bold mb-2"
                 for="name"
               >
                 Name
               </label>
               <input
-                class="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="input shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="name"
                 type="text"
                 placeholder="Task name"
               />
             </div>
-            <div class="mb-4">
+
+            <div className="mb-4">
               <label
-                class="block text-gray-700 text-sm font-bold mb-2"
-                for="description"
+                className="block text-gray-700 text-sm font-bold mb-2"
+                for="duedate"
               >
-                Description
+                Due Date {selectedDate}
+                <span className={dateExpired ? 'expired-msg alert' : 'hide'}>
+                  Task has already expired
+                </span>
               </label>
+
               <input
-                class="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="description"
+                className={
+                  dateExpired
+                    ? 'input alert shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                    : 'input shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                }
+                // className="input shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="duedate"
+                type="date"
+                defaultValue={date}
+                placeholder="Task name"
+                onChange={handleDateChange}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                for="notes"
+              >
+                Task notes
+              </label>
+              <textarea
+                className="input shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="notes"
                 type="text"
                 placeholder="Say something about the task"
               />
