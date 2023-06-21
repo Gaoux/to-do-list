d61cc96e96c6fd91647ba932f031eff39f151ce1
Gustavo Parra <gustavoparratanoux@gmail.com>
2023-06-20 20:08:23 -0500

    Improved add task modal;

--- src/components/AddTaskModal.js
+++ src/components/AddTaskModal.js
@@ -1,5 +1,6 @@
 import Modal from 'react-bootstrap/Modal';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 function AddTaskModal(props) {
   const handleClose = () => props.setShow(false);
   // const handleShow = () => props.setShow(true);
@@ -11,76 +12,55 @@ function AddTaskModal(props) {
         onHide={handleClose}
         backdrop="static"
         keyboard={false}
+        className="add-task-modal"
       >
-        <Modal.Header closeButton>
+        <Modal.Header className="add-task-modal__header">
           <Modal.Title>Add a task</Modal.Title>
+          <FontAwesomeIcon
+            icon={faX}
+            onClick={handleClose}
+            style={{ cursor: 'pointer' }}
+          />
         </Modal.Header>
-        <Modal.Body>
-          {/* <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="name@example.com"
-                autoFocus
+        <Modal.Body className="add-task-modal__body">
+          <form class=" rounded px-8 pt-6 pb-8 mb-4">
+            <div class="mb-4">
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2"
+                for="name"
+              >
+                Name
+              </label>
+              <input
+                class="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="name"
+                type="text"
+                placeholder="Task name"
               />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Example textarea</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-          </Form> */}
-
-          <form id="addtask" class="w-full max-w-sm">
-            <div class="md:flex md:items-center mb-6">
-              <div class="md:w-1/3">
-                <label
-                  class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                  for="inline-task-name"
-                >
-                  Name
-                </label>
-              </div>
-              <div class="md:w-2/3">
-                <input
-                  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                  id="inline-task-name"
-                  type="text"
-                  value=""
-                />
-              </div>
             </div>
-            <div class="md:flex md:items-center mb-6">
-              <div class="md:w-1/3">
-                <label
-                  class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                  for="inline-description"
-                >
-                  Description
-                </label>
-              </div>
-              <div class="md:w-2/3">
-                <input
-                  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                  id="inline-description"
-                  type="text"
-                  value=""
-                />
-              </div>
+            <div class="mb-4">
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2"
+                for="description"
+              >
+                Description
+              </label>
+              <input
+                class="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="description"
+                type="text"
+                placeholder="Say something about the task"
+              />
             </div>
           </form>
-        </Modal.Body>
-        <Modal.Footer>
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
             form="addtask"
           >
-            ADD TASK
+            add task
           </button>
-        </Modal.Footer>
+        </Modal.Body>
+        {/* <Modal.Footer className="add-task-modal__footer"></Modal.Footer> */}
       </Modal>
     </>
   );
