a5d32efec625a06d6545d483f6d6ec7fcbee4b1f
Gustavo Parra <gustavoparratanoux@gmail.com>
2023-06-23 16:51:05 -0500

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Navbar } from './components/Navbar';
-import { Home } from './pages/home';
+import { Home } from './pages/Home';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import React, { useState } from 'react';
 
