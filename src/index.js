2bd76ca9c42c27434c028994673bb5dbf0ea835b
Gustavo Parra <gustavoparratanoux@gmail.com>
2023-06-23 20:24:27 -0500

    Added hashrouter instead of browserRouter Trying to fix the gh-pages not working

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/sass/styles.scss';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <HashRouter>
     <App />
-  </Router>
+  </HashRouter>
 );
